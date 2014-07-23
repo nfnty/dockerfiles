78d9009f202c84857917f2c97949fee1c6cd0cc1
nfnty <8@nfnty.se>
2014-07-23 23:15:36 +0200

    local instead of git

--- arch-makepkg/build.sh
+++ arch-makepkg/build.sh
@@ -6,7 +6,7 @@ if [[ $2 == '1' ]]; then
 	curl -O "https://aur.archlinux.org/packages/ra/$1/${1}.tar.gz"
 	tar -xzf "${1}.tar.gz"
 elif [[ $2 == '2' ]]; then
-	cp -r /srv/builder/git/$1 /srv/builder/makepkg/
+	cp -r /srv/builder/local/$1 /srv/builder/makepkg/
 fi
 
 cd /srv/builder/makepkg/$1
