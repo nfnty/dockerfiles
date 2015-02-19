7ffc3fa8cfa797c211ea4db976601a45b451f510
nfnty <git@nfnty.se>
2015-02-20 00:20:54 +0100

    Use automated build process

--- arch-mini/mkimage.sh
+++ arch-mini/mkimage.sh
@@ -12,55 +12,54 @@ if ! hash pacstrap &>/dev/null; then
     exit 1
 fi
 
-while true; do
-    read -e -p 'Name of image? ' CONTAINERNAME
-    if [[ -n "$CONTAINERNAME" ]]; then
-        break
-    else
-        echo "Name can't be empty."
-        continue
-    fi
-done
-
 SCRIPTDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-ROOTFS=$(mktemp -d ${TMPDIR:-/var/tmp}/rootfs-archlinux-XXXXXXXXXX)
+ROOTFS=$(mktemp --directory ${TMPDIR:-/var/tmp}/rootfs-archlinux-XXXXXXXXXX)
 chmod 755 "$ROOTFS"
 
 function cleanup {
     echo "Removing $ROOTFS"
-    rm -r "$ROOTFS"
+    rm --recursive "$ROOTFS"
 }
 trap cleanup EXIT
 
 PKGS='bash filesystem glibc pacman shadow'
 
-pacstrap -C "$SCRIPTDIR/etc/pacman.conf" -c -d -G "$ROOTFS" $PKGS haveged procps-ng
-arch-chroot "$ROOTFS" /usr/bin/bash -c 'haveged -w 1024; pacman-key --init; pkill -x haveged; pacman-key --populate archlinux; pkill -x gpg-agent; pacman -Rsn --noconfirm haveged procps-ng'
+pacstrap -c -d -G -M "$ROOTFS" $PKGS haveged procps-ng sed
+install --mode=644 "$SCRIPTDIR/etc/pacman.d/mirrorlist" "$ROOTFS/etc/pacman.d/"
+install --mode=644 "$SCRIPTDIR/etc/locale.conf" "$ROOTFS/etc/"
+install --mode=644 "$SCRIPTDIR/etc/locale.gen" "$ROOTFS/etc/"
 
-install -m644 "$SCRIPTDIR/etc/locale.conf" "$ROOTFS/etc/"
-install -m644 "$SCRIPTDIR/etc/locale.gen" "$ROOTFS/etc/"
-install -m644 "$SCRIPTDIR/etc/pacman.conf" "$ROOTFS/etc/"
-arch-chroot "$ROOTFS" /usr/bin/bash -c "ln -s /usr/share/zoneinfo/UTC /etc/localtime"
-arch-chroot "$ROOTFS" /usr/bin/bash -c "pacman -S --noconfirm --asdeps --needed sed gzip"
-arch-chroot "$ROOTFS" locale-gen
+arch-chroot "$ROOTFS" /usr/bin/bash << 'EOF'
+haveged --write=1024
+pacman-key --init
+pkill --exact haveged
+pacman-key --populate archlinux
+pkill --exact gpg-agent
+
+sed --in-place 's/^CheckSpace/#CheckSpace/g' /etc/pacman.conf
+
+ln --symbolic /usr/share/zoneinfo/UTC /etc/localtime
+locale-gen
+
+pacman --remove --recursive --nosave --noconfirm haveged procps-ng sed
+EOF
 
 DEV="$ROOTFS/dev"
-rm -rf "$DEV"
-mkdir -p "$DEV"
-mknod -m 666 "$DEV/null" c 1 3
-mknod -m 666 "$DEV/zero" c 1 5
-mknod -m 666 "$DEV/random" c 1 8
-mknod -m 666 "$DEV/urandom" c 1 9
-mkdir -m 755 "$DEV/pts"
-mkdir -m 1777 "$DEV/shm"
-mknod -m 666 "$DEV/tty" c 5 0
-mknod -m 600 "$DEV/console" c 5 1
-mknod -m 666 "$DEV/tty0" c 4 0
-mknod -m 666 "$DEV/full" c 1 7
-mknod -m 600 "$DEV/initctl" p
-mknod -m 666 "$DEV/ptmx" c 5 2
-ln -sf /proc/self/fd "$DEV/fd"
+rm --recursive --force "$DEV"
+mkdir --parents "$DEV"
+mknod --mode=666 "$DEV/null" c 1 3
+mknod --mode=666 "$DEV/zero" c 1 5
+mknod --mode=666 "$DEV/random" c 1 8
+mknod --mode=666 "$DEV/urandom" c 1 9
+mkdir --mode=755 "$DEV/pts"
+mkdir --mode=1777 "$DEV/shm"
+mknod --mode=666 "$DEV/tty" c 5 0
+mknod --mode=600 "$DEV/console" c 5 1
+mknod --mode=666 "$DEV/tty0" c 4 0
+mknod --mode=666 "$DEV/full" c 1 7
+mknod --mode=600 "$DEV/initctl" p
+mknod --mode=666 "$DEV/ptmx" c 5 2
+ln --symbolic --force /proc/self/fd "$DEV/fd"
 
-tar --numeric-owner --xattrs --acls -C "$ROOTFS" -c . | docker import - "$CONTAINERNAME"
-docker run --rm -t "$CONTAINERNAME" echo Success.
+tar --numeric-owner --xattrs --acls --xz --create --directory="$ROOTFS" --file="$SCRIPTDIR/arch-mini.tar.xz" .
