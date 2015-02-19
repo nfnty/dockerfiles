16491cbc45e1d679875a602b9dcdd852424f3ab9
nfnty <git@nfnty.se>
2015-02-19 11:09:21 +0100

    use separate config files

--- arch-mini/mkimage.sh
+++ arch-mini/mkimage.sh
@@ -35,7 +35,7 @@ trap cleanup EXIT
 
 PKGS='bash filesystem glibc pacman shadow'
 
-pacstrap -C "$SCRIPTDIR/pacman.conf" -c -d -G "$ROOTFS" "$PKGS"
+pacstrap -C "$SCRIPTDIR/etc/pacman.conf" -c -d -G "$ROOTFS" "$PKGS"
 arch-chroot "$ROOTFS" /usr/bin/bash -c 'pacman -S --noconfirm haveged procps-ng; haveged -w 1024; pacman-key --init; pkill -x haveged; pacman -Rsn --noconfirm haveged procps-ng; pacman-key --populate archlinux; pkill gpg-agent'
 
 while true; do
@@ -53,49 +53,27 @@ while true; do
     fi
 done
 
+cp "$SCRIPTDIR/etc/locale.conf" "$ROOTFS/etc/"
+cp "$SCRIPTDIR/etc/locale.gen" "$ROOTFS/etc/"
+cp "$SCRIPTDIR/etc/pacman.conf" "$ROOTFS/etc/"
 arch-chroot "$ROOTFS" /usr/bin/bash -c "ln -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime"
-
-cat > "$ROOTFS/etc/locale.conf" << EOF
-LANG="en_US.UTF-8"
-LANGUAGE="en_US:en"
-LC_CTYPE="en_US.UTF-8"
-LC_NUMERIC="en_US.UTF-8"
-LC_TIME="en_DK.UTF-8"
-LC_COLLATE="en_US.UTF-8"
-LC_MONETARY="en_US.UTF-8"
-LC_MESSAGES="en_US.UTF-8"
-LC_PAPER="en_DK.UTF-8"
-LC_NAME="en_US.UTF-8"
-LC_ADDRESS="en_US.UTF-8"
-LC_TELEPHONE="en_US.UTF-8"
-LC_MEASUREMENT="en_DK.UTF-8"
-LC_IDENTIFICATION="en_US.UTF-8"
-EOF
-
-cat > "$ROOTFS/etc/locale.gen" << EOF
-en_US.UTF-8 UTF-8
-en_DK.UTF-8 UTF-8
-EOF
-
 arch-chroot "$ROOTFS" /usr/bin/bash -c "pacman -S --noconfirm --asdeps --needed sed gzip"
 arch-chroot "$ROOTFS" locale-gen
 arch-chroot "$ROOTFS" /usr/bin/bash -c "pacman -Rsn --noconfirm gzip"
-
-cp "$SCRIPTDIR/pacman.conf" "$ROOTFS/etc/"
 
 DEV="$ROOTFS/dev"
 rm -rf "$DEV"
 mkdir -p "$DEV"
 mknod -m 666 "$DEV/null" c 1 3
 mknod -m 666 "$DEV/zero" c 1 5
-mknod -m 666 "$DEV/full" c 1 7
 mknod -m 666 "$DEV/random" c 1 8
 mknod -m 666 "$DEV/urandom" c 1 9
 mkdir -m 755 "$DEV/pts"
 mkdir -m 1777 "$DEV/shm"
 mknod -m 666 "$DEV/tty" c 5 0
-mknod -m 666 "$DEV/tty0" c 4 0
 mknod -m 600 "$DEV/console" c 5 1
+mknod -m 666 "$DEV/tty0" c 4 0
+mknod -m 666 "$DEV/full" c 1 7
 mknod -m 600 "$DEV/initctl" p
 mknod -m 666 "$DEV/ptmx" c 5 2
 ln -sf /proc/self/fd "$DEV/fd"
