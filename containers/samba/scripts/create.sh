79ec3613ffd968188711357f86ac9d4263a1fdd5
nfnty <git@nfnty.se>
2015-03-22 01:44:25 +0100

    added arch-samba image and container

---
+++ containers/samba/scripts/create.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+set -o errexit -o noclobber -o noglob -o nounset -o pipefail
+
+SCRIPTDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+CNAME='samba'
+UGID=140000
+
+BASEPATH='/srv/docker/samba'
+CONFIGPATH="$BASEPATH/config"
+DATAPATH="$BASEPATH/data"
+SHARE1='/mnt/1/share'
+
+TESTSCRIPT="$SCRIPTDIR/../../scripts/test_file.py"
+
+"$TESTSCRIPT" 'fr' "$UGID" "$CONFIGPATH/smb.conf"
+"$TESTSCRIPT" 'drwxUG' '0' "$DATAPATH"
+"$TESTSCRIPT" 'drwxUGg' "$UGID" "$SHARE1"
+
+docker create \
+    --volume="$CONFIGPATH:/samba/config:ro" \
+    --volume="$DATAPATH:/samba/data" \
+    --volume="$SHARE1:/share/1" \
+    --net=none \
+    --name="$CNAME" \
+    nfnty/arch-samba
