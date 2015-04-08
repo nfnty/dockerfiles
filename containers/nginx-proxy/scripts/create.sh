7bb8001fd083d4b37135d33de03e2a8d8c1c404f
nfnty <git@nfnty.se>
2015-04-08 19:49:50 +0200

    Added container nginx-proxy
    Made for using nginx builtin proxy capabilites

---
+++ containers/nginx-proxy/scripts/create.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+set -o errexit -o noclobber -o noglob -o nounset -o pipefail
+
+SCRIPTDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+CNAME='nginx-proxy'
+UGID=160000
+PRIMPATH='/nginx'
+
+source "${SCRIPTDIR}/../../scripts/variables.sh"
+
+CONFIGPATH="${HOSTPATH}/config"
+CRYPTOPATH="${HOSTPATH}/crypto"
+
+perm_root "${HOSTPATH}" '-maxdepth 0'
+perm_root "${CONFIGPATH}"
+perm_root "${CRYPTOPATH}"
+
+docker create \
+    --volume="${CONFIGPATH}:${PRIMPATH}/config:ro" \
+    --volume="${CRYPTOPATH}:${PRIMPATH}/crypto:ro" \
+    --net=none \
+    --dns="${DNSSERVER}" \
+    --name="${CNAME}" \
+    nfnty/arch-nginx:latest
