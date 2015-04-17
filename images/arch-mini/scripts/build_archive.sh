876a5246e480a8851a68f4507deab4a7029a49c5
nfnty <git@nfnty.se>
2015-04-17 12:24:31 +0200

    arch-mini: Minor refactoring, /dev is handled by docker

--- images/arch-mini/scripts/build_archive.sh
+++ images/arch-mini/scripts/build_archive.sh
@@ -2,7 +2,10 @@
 
 set -o errexit -o noclobber -o noglob -o nounset -o pipefail
 
-umask 022
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+DOCKERDIR="${SCRIPTDIR}/../latest"
+ROOTFS="${TMPDIR}/docker_arch-mini_archive"
 
 if [[ "${UID}" != '0' ]]; then
     echo 'Needs to be run as root.'
@@ -12,11 +15,13 @@ if ! hash pacstrap &>/dev/null; then
     echo 'Could not find pacstrap. Run pacman -S arch-install-scripts'
     exit 1
 fi
+if [[ -d "${ROOTFS}" ]]; then
+    echo "${ROOTFS} already exists!"
+    exit 1
+fi
 
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-ROOTFS=$(mktemp --directory "${TMPDIR}/rootfs-archlinux-XXXXXXXXXX")
-chmod 755 "${ROOTFS}"
+umask 022
+install --directory --owner=root --group=root --mode=755 "${ROOTFS}"
 
 function cleanup {
     echo "Removing ${ROOTFS}"
@@ -24,28 +29,22 @@ function cleanup {
 }
 trap cleanup EXIT
 
-PKGS='bash coreutils curl filesystem findutils gcc-libs glibc gzip pacman shadow tar xz'
+PKGS=(
+    'bash'
+    'coreutils'
+    'curl'
+    'filesystem'
+    'findutils'
+    'gcc-libs'
+    'glibc'
+    'gzip'
+    'pacman'
+    'shadow'
+    'tar'
+    'xz'
+)
 
-pacstrap -c -d -G -M "${ROOTFS}" ${PKGS}
-
-rm --recursive "${ROOTFS}/dev"
-mkdir "${ROOTFS}/dev"
-cd "${ROOTFS}/dev"
-mkdir --mode=755 pts
-mkdir --mode=1777 shm
-mknod --mode=666 null c 1 3
-mknod --mode=666 zero c 1 5
-mknod --mode=666 full c 1 7
-mknod --mode=666 random c 1 8
-mknod --mode=666 urandom c 1 9
-mknod --mode=666 tty c 5 0
-mknod --mode=666 tty0 c 4 0
-mknod --mode=600 console c 5 1
-mknod --mode=600 initctl p
-mknod --mode=666 ptmx c 5 2
-ln --symbolic '/proc/self/fd' fd
-
-DOCKERDIR="${SCRIPTDIR}/../latest"
+pacstrap -c -d -G -M "${ROOTFS}" ${PKGS[@]}
 
 cd "${DOCKERDIR}/bootstrap"
 DATE="$(date --iso-8601)"
