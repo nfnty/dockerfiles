2eb049ae584d069350387ba352641bce065e7405
nfnty <git@nfnty.se>
2015-04-23 14:13:12 +0200

    scripts: Container update script refactored using Python

--- scripts/update_all_containers.sh
+++ scripts/update_all_containers.sh
@@ -4,20 +4,19 @@ set -o errexit -o noclobber -o noglob -o nounset -o pipefail
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-UPDCON="${SCRIPTDIR}/update_container.sh"
+UPDCON="${SCRIPTDIR}/update_container.py"
 
 cd "${SCRIPTDIR}"
 
-"${UPDCON}"       'transmission'    'on'
-"${UPDCON}"       'nginx-proxy'     'on'
-"${UPDCON}"       'nginx-mirror'    'on'
-"${UPDCON}"       'samba'           'on'
-"${UPDCON}"       'kibana'          'off'
-"${UPDCON}"       'logstash'        'off'
-"${UPDCON}"       'elasticsearch'   'on'
+"${UPDCON}"       'transmission'
+"${UPDCON}"       'nginx-proxy'
+"${UPDCON}"       'nginx-mirror'
+"${UPDCON}"       'samba'
+"${UPDCON}" --off 'kibana'
+"${UPDCON}" --off 'logstash'
+"${UPDCON}"       'elasticsearch'
 systemctl start docker_logstash
 systemctl start docker_kibana
 
-"${UPDCON}"       'psql-openhab'   'off'
-"${UPDCON}"       'openhab'        'off'
-# "${UPDCON}"       'makepkg'        'off'
+"${UPDCON}"       'psql-openhab'
+"${UPDCON}"       'openhab'
