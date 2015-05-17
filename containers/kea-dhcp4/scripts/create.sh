2013849bd314321a84a70230f45fb4ed7ad37d3c
nfnty <git@nfnty.se>
2015-05-17 16:10:07 +0200

    Added kea

---
+++ containers/kea-dhcp4/scripts/create.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+set -o errexit -o noclobber -o noglob -o nounset -o pipefail
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CNAME='kea-dhcp4' UGID='0' PRIMPATH='/kea'
+MEMORY='1G' CPU_SHARES='2048'
+
+source "${SCRIPTDIR}/../../scripts/variables.sh"
+
+CONFIGPATH="${HOSTPATH}/config"
+DATAPATH="${HOSTPATH}/data"
+CACHEPATH="${HOSTPATH}/cache"
+
+perm_root "${HOSTPATH}" '-maxdepth 0'
+perm_root "${CACHEPATH}"
+perm_root "${CONFIGPATH}"
+perm_root "${DATAPATH}"
+
+docker create \
+    --read-only \
+    --volume="${CONFIGPATH}:${PRIMPATH}/config:ro" \
+    --volume="${DATAPATH}:${PRIMPATH}/data:rw" \
+    --volume="${CACHEPATH}:${PRIMPATH}/cache:rw" \
+    --net='host' \
+    --name="${CNAME}" \
+    --memory="${MEMORY}" \
+    --memory-swap='-1' \
+    --cpu-shares="${CPU_SHARES}" \
+    --entrypoint='/usr/bin/kea-dhcp4' \
+    nfnty/arch-kea:latest \
+    -c '/kea/config/kea.conf'
