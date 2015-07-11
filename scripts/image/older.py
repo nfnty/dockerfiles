e6505924e4d2b0f34bc994dafe80ec448931a4c5
nfnty <git@nfnty.se>
2015-07-11 04:11:17 +0200

    Image update: Add ability to set age

--- scripts/image/older.py
+++ scripts/image/older.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-''' Print True if age of image is older than 1h '''
+''' Print True if age of image is older than amount of seconds '''
 from datetime import datetime
 import docker
 import sys
 
 CLIENT = docker.Client(base_url='unix://run/docker.sock', version='auto')
+INAME = sys.argv[1]
+SECONDS = int(sys.argv[2])
 
 def main():
     ''' Main '''
-    if 1 >= len(sys.argv):
-        sys.exit(1)
-    created = CLIENT.inspect_image(sys.argv[1])['Created']
+    created = CLIENT.inspect_image(INAME)['Created']
     created = int(
         datetime.strptime(
             created.split('.')[0], '%Y-%m-%dT%H:%M:%S'
         ).strftime('%s')
     )
-    print(3600 < (int(datetime.utcnow().strftime('%s')) - created))
+    print(SECONDS < (int(datetime.utcnow().strftime('%s')) - created))
 
 if __name__ == '__main__':
     main()
