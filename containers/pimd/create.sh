611b76aa05f5ae2acc36e45976eccdee3368fb25
nfnty <git@nfnty.se>
2015-10-29 12:39:08 +0100

    Added pimd

---
+++ containers/pimd/create.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+set -o errexit -o noclobber -o noglob -o nounset -o pipefail
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CNAME="${1}"
+
+source "${SCRIPTDIR}/var.sh"
+
+docker create \
+    --read-only \
+    --volume="${CONFIGPATH}:/etc/pimd:ro" \
+    --volume="${RUNPATH}:/var/run:rw" \
+    --cap-drop 'ALL' \
+    --cap-add 'NET_ADMIN' \
+    --cap-add 'NET_RAW' \
+    --net='host' \
+    --name="${CNAME}" \
+    --memory="${MEMORY}" \
+    --memory-swap='-1' \
+    --cpu-shares="${CPU_SHARES}" \
+    --entrypoint='/usr/bin/pimd' \
+    nfnty/arch-pimd:latest \
+    '--foreground' '--disable-vifs' '--debug'
---
+++ containers/pimd/var.sh
@@ -0,0 +1,8 @@
+UGID='300000'
+MEMORY='1G'
+CPU_SHARES='2048'
+
+source "${SCRIPTDIR}/../_misc/variables.sh"
+
+CONFIGPATH="${HOSTPATH}/config"
+RUNPATH="${HOSTPATH}/run"
