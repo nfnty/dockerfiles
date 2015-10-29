769eaa44eb413679b6632aaf54fd894e0f94fbda
nfnty <git@nfnty.se>
2015-10-29 12:39:15 +0100

    Added avahi

---
+++ containers/avahi/create.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+set -o errexit -o noclobber -o noglob -o nounset -o pipefail
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CNAME="${1}"
+
+source "${SCRIPTDIR}/var.sh"
+
+docker create \
+    --read-only \
+    --volume="${CONFIGPATH}:/etc/avahi:ro" \
+    --cap-drop 'ALL' \
+    --net='host' \
+    --name="${CNAME}" \
+    --memory="${MEMORY}" \
+    --memory-swap='-1' \
+    --cpu-shares="${CPU_SHARES}" \
+    nfnty/arch-avahi:latest
---
+++ containers/avahi/var.sh
@@ -0,0 +1,7 @@
+UGID='310000'
+MEMORY='1G'
+CPU_SHARES='2048'
+
+source "${SCRIPTDIR}/../_misc/variables.sh"
+
+CONFIGPATH="${HOSTPATH}/config"
