86e10a507e0f1d6d91569d8937284a635df149f6
nfnty <git@nfnty.se>
2016-01-04 09:23:58 +0100

    Remove UGID2

--- containers/plex/perm.sh
+++ containers/plex/perm.sh
@@ -11,6 +11,3 @@ source "${SCRIPTDIR}/../_misc/permissions.sh"
 
 perm_user_rw "${PATH_LIB}"
 perm_user_rw "${PATH_TMP}"
-
-perm_custom "${PATH_SHARE1}" '140000' '140000' 'u=rwX,g=rwXs,o=' "-path ${PATH_SHARE1}/torrent -prune -or -type d"
-perm_custom "${PATH_SHARE1}" '140000' '140000' 'u=rwX,g=rwX,o=' "-path ${PATH_SHARE1}/torrent -prune -or -type f"
